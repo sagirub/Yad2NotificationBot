1f57a528e5b06bc228fcb7747c96fa3d131cf788
sagirub <sagirub2@gmail.com>
2022-12-21 21:59:52 +0200

    minor changes

--- bot/add_search.py
+++ bot/add_search.py
@@ -15,8 +15,9 @@ from connectors.db import Search
 # Init logger
 logger = logging.getLogger(__name__)
 
+
 async def add_search(update: Update, context: CallbackContext.DEFAULT_TYPE) -> int:
-    """Send a message when the command /search is issued or the user type the add search buttom in menu."""
+    """Send a message when the command /search is issued or the user type the add search button in menu."""
 
     await update.callback_query.answer()
     await context.bot.send_message(
@@ -26,6 +27,7 @@ async def add_search(update: Update, context: CallbackContext.DEFAULT_TYPE) -> i
 
     return ADD_SEARCH_LINK
 
+
 async def add_search_link(update: Update, context: CallbackContext.DEFAULT_TYPE) -> int:
     """Get the search link from the user after the command /search is issued"""
     search_link = update.message.text
@@ -34,7 +36,7 @@ async def add_search_link(update: Update, context: CallbackContext.DEFAULT_TYPE)
     parsed_url = parse.urlparse(search_link)
 
     # TODO: MAYBE NEED A DEEP CHECK HERE? BROWSE THE LINK AND VALIDATE THAT IS A REAL SEARCH?
-    if (parsed_url.netloc != YAD2_VALID_NETLOC):
+    if parsed_url.netloc != YAD2_VALID_NETLOC:
         await update.message.reply_text(text=ADD_SEARCH_LINK_ERROR_MESSAGE)
 
         return ADD_SEARCH_LINK
@@ -44,6 +46,7 @@ async def add_search_link(update: Update, context: CallbackContext.DEFAULT_TYPE)
     await update.message.reply_text(text=ADD_SEARCH_LINK_MESSAGE)
 
     return ADD_SEARCH_NAME
+
 
 async def add_search_name(update: Update, context: CallbackContext.DEFAULT_TYPE) -> int:
     """Get the name of the search after getting the search link"""
@@ -65,10 +68,10 @@ async def add_search_name(update: Update, context: CallbackContext.DEFAULT_TYPE)
                     search_name: {search_name}, search_link: {search_link}')
     except Exception as error:
         message = ADD_SEARCH_FAIL_END_MESSAGE
-        logger.error(f'filed to add a new search. user_id:{update.message.from_user.id}, \
+        logger.error(f'failed to add a new search. user_id:{update.message.from_user.id}, \
                             user_name: {update.message.forward_sender_name}, \
                             search_name: {search_name}, search_link: {search_link}')
-
+        logger.error(error)
 
     # delete user search link from context
     del context.user_data['search_link']
