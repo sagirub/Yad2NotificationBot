b33f37619bf761821797300562d6ecb91185074e
sagirub <sagirub2@gmail.com>
2024-07-23 12:08:08 +0300

    created the db table in the sam template file instead of the code

--- src/connectors/db/sync.py
+++
@@ -1,5 +0,0 @@
-from .models import Search
-
-""" creates the search table in dynamodb if not exists yet"""
-if not Search.exists():
-    Search.create_table(wait=True, read_capacity_units=1, write_capacity_units=1)
